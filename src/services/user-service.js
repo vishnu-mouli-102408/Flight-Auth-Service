80e3afa583faf37c92a559ffe8eac1934d9a33a4
Ganivada Mouli <vishnumouli0@gmail.com>
2023-10-18 21:05:18 +0530

    Added validation functions in service layer

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,5 +1,10 @@
 const UserRepository = require("../repository/user-repository");
 
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const { JWT_SECRET } = require("../config/serverConfig");
+
 class UserService {
   constructor() {
     this.userRepository = new UserRepository();
@@ -11,6 +16,35 @@ class UserService {
       return user;
     } catch (error) {
       console.log("Something went wrong in Service Layer");
+      throw error;
+    }
+  }
+
+  verifyPassword(inputPassword, encryptedPassword) {
+    try {
+      return bcrypt.compareSync(inputPassword, encryptedPassword);
+    } catch (error) {
+      console.log("Something went wrong in Bcrypt Password Verification.");
+      throw error;
+    }
+  }
+
+  createToken(user) {
+    try {
+      const response = jwt.sign(user, JWT_SECRET, { expiresIn: "2 days" });
+      return response;
+    } catch (error) {
+      console.log("Something went wrong in Token Creation");
+      throw error;
+    }
+  }
+
+  verifyToken(token) {
+    try {
+      const response = jwt.verify(token, JWT_SECRET);
+      return response;
+    } catch (error) {
+      console.log("Something went wrong in Token Validation");
       throw error;
     }
   }
